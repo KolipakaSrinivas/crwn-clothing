495ad7b44ecb207d7a6d039dfa3f32cc32b15bbd
Srinivas <kolipakasrinivas456@gmail.com>
2022-10-27 17:20:57 +0530

    created component folder add so many folders and files

---
+++ src/components/pages/Homepage.component.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Homepage.style.scss'
+import Directory from '../directory-menu/directory-menu.component'
+
+const Homepage = () =>{
+    return(
+        <div className='homepage'>
+            <Directory />
+           
+        </div>
+    )
+}
+
+
+export default Homepage
