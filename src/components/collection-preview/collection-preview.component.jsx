b5e9a2fb8c59d5b97bb6bc7225ed93ec09822edd
Srinivas <kolipakasrinivas456@gmail.com>
2022-11-03 21:28:00 +0530

    chaned the lessthan symbol

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -7,7 +7,7 @@ const CollectionPreview = ({title,items,routeName}) =>{
             <h1 className='title'>{title.toUpperCase()}</h1>
             <div className='preview'>
                 {
-                    items.filter((item,index)=>index > 4)
+                    items.filter((item,index)=>index < 4)
                     .map(items=>(
                            <div key={items.id}>{items.name}</div> 
                         )
