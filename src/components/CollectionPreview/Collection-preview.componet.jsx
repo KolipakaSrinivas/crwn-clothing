370e3db6b4ae470dcaaedeb1725378886c45cb7d
Srinivas <kolipakasrinivas456@gmail.com>
2022-11-07 13:15:03 +0530

    created collection-items and  2 hours watste time on imageUrl missing l letter

--- src/components/CollectionPreview/Collection-preview.componet.jsx
+++ src/components/CollectionPreview/Collection-preview.componet.jsx
@@ -1,15 +1,20 @@
 import React from 'react';
+import CollectionItems from '../Collection-items/Collection-items.componet';
+import './collection-preview.style.scss'
 
 const CollectionPreview = ({title,items}) =>{
     return(
-        <div>
-            <h1 className='title'>{title}</h1>
+        <div className='collection-preview'>
+            <h1 className='title' >{title.toUpperCase()}</h1>
+           <div className='preview'>
             {
                 items.filter((items,index)=> index < 4 )
-                .map((items)=>(
-                    <div>{items.name}</div>
+                .map(({id,...otheritemsprops})=>(
+                    <CollectionItems key={id} {...otheritemsprops} />
                 ))
+            
             }
+            </div>
         </div>
     )
 } 
