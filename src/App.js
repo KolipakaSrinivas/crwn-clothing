f3267d7d307c2f3d5b8bf4cccfeae7e5f77f3664
Srinivas <kolipakasrinivas456@gmail.com>
2022-11-10 21:21:04 +0530

    sign in and up deleted

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import './App.scss';
 import { Routes,Route } from 'react-router-dom';
 
 
-import SingInSingUp from '../src/pages/sing-in-.sing-up/Sign-in.sing-up.component'
 import Header from './components/header/Header.component'
 import Homepage from './pages/HomePage/Homepage.component'
 import ShopPage from './pages/shopePage/Shop.component';
@@ -16,7 +15,6 @@ function App() {
       <Routes>
         <Route path='/' element={<Homepage/>} />
         <Route path='/shop' element={<ShopPage/>} />
-        <Route path='/SingInSingUp' element={<SingInSingUp/>}/>
       </Routes>
       </div>
   );
--- src/pages/sing-in-.sing-up/text.js
+++
@@ -1,2 +0,0 @@
-//how toconnect to monogodb ?
-//connect monogodb to node js
